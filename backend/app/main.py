3806ff83ec40a92b296dbcb9a98502cdff9e35b3
Sergio <sergio.simoes.info@gmail.com>
2026-08-19 16:33:53 +0100

    Added basic React front end

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 
 from app.users import models as user_models
@@ -15,6 +16,14 @@ from app.ai import router as ai_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router.router)
 app.include_router(expense_router.router)
